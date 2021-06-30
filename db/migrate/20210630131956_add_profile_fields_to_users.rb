59794dbbed4bc0a6b9945dc04ec67069e413c3e3
ZawadiSumba66 <rosesumba98@gmail.com>
2021-06-30 17:22:15 +0300

    Initialize devise and add migrations for users

---
+++ db/migrate/20210630131956_add_profile_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddProfileFieldsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :image, :string
+  end
+end
