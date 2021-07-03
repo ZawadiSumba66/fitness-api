b93f0fb8b73081b3bb46b573bfb22507abbfe5c0
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-03 13:30:02 +0300

    Setup devise jwt config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,7 +280,7 @@ Devise.setup do |config|
   config.warden do |manager|
     # manager.intercept_401 = false
     manager.strategies.add :jwt, Devise::Strategies::JWT
-    manager.default_strategies(scope: :user).unshift :some_external_strategy
+    # manager.default_strategies(scope: :user).unshift :some_external_strategy
     manager.default_strategies(scope: :user).unshift :jwt
   end
 
