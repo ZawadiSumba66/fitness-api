3c2f0aa1a66f3edcc80ffba0088c0b4d6336bddc
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-06 09:19:23 +0300

    Cofigured routes for the direct upload controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
           post 'favorite', to: 'tips#favorite'
           post 'unfavourite', to: 'tips#unfavourite'
         end
+        post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
       end
     end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
