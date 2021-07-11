e2258dc362551d497eb87929e083ad83d2668c32
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-11 16:47:58 +0300

    Create necessary tests for models and requests

--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Tip, type: :model do
-    it { should have_many(:favorites) }
-    it { should belong_to(:user) }
-    it { should have_one_attached(:images) }
-    it { should have_many(:favoritors) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:benefits) }
-    it { should validate_presence_of(:instructions) }
+  it { should have_many(:favorites) }
+  it { should belong_to(:user) }
+  it { should have_one_attached(:images) }
+  it { should have_many(:favoritors) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:benefits) }
+  it { should validate_presence_of(:instructions) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it { should have_many(:tips) }
-    it { should have_many(:favorites) }
-    it { should have_one_attached(:avatar) }
-    it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_uniqueness_of(:username) }
+  it { should have_many(:tips) }
+  it { should have_many(:favorites) }
+  it { should have_one_attached(:avatar) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:username) }
 end
