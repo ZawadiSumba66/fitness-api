34dfb658231f8c912cb74b53cffd61273052491e
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-12 17:39:18 +0300

    Proper cloudinary setup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,20 +2,12 @@ class Api::V1::UsersController < ApiController
   skip_before_action :authenticate_user!
 
   def create
-    if params[:user][:email]
-      duplicate_user = User.find_by_email(params[:email])
-      unless duplicate_user.nil?
-        render json: { message: 'Duplicate email. A user already exists with that email address.' }, status: 409
-        return
-      end
-    end
+    @user = User.new(user_params)
 
-    user = User.new(user_params)
-
-    if user.save
-      render json: { token: JsonWebToken.encode(sub: user.id) }, status: 200
+    if @user.save
+      render json: { token: JsonWebToken.encode(sub: @user.id) }, status: 200
     else
-      render json: { message: user.errors.full_messages }, status: 400
+      render json: { message: @user.errors.full_messages }, status: 400
     end
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require_relative "application"
-
+require 'carrierwave/orm/activerecord'
+require 'carrierwave'
 # Initialize the Rails application.
 Rails.application.initialize!
 # config.gem 'carrierwave', :version => '~> 0.4.10'
